7ee09f764e3b5e2b9a0fb28cbc7f19b3e6748edb
artorias <agustincastro,irb@gmail.com>
2023-02-28 21:12:41 -0300

    project tic tac toc funcional 1.0.0

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,15 +1,76 @@
+import confetti from 'canvas-confetti'
+import { useState } from 'react'
 import './App.css'
-
-const TURNS = {
-  X: "x",
-  O: "o"
-}
+import Square from "./components/Square.jsx";
+import WinerModal from './components/WinnerModal.jsx';
+import { TURNS } from "./constants.js";
+import { checkWinnerFrom, checkEndGameFrom } from './logic/board.js';
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
 
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const updateBoard = (index) => {
+    if (board[index] || winner)return
+    
+    const newBoard = [...board]
+    
+    newBoard[index] = turn
+    
+    setBoard(newBoard)
+    
+    const newturn = turn === TURNS.X ? TURNS.O : TURNS.X
+    
+    setTurn(newturn)
+
+    const newWinner = checkWinnerFrom(newBoard)
+    
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGameFrom(newBoard)) {
+      setWinner(false)
+    }
+  }
 
   return (
-    <h1>tic tac toe</h1>
+    <main className='board'>
+      <h1>tic tac toe</h1>
+      
+      <button onClick={resetGame}>Reset del juego</button>
+
+      <section className='game'>
+        {
+          board.map((square, index) => {
+            return(
+              <Square key={index} index={index} updateBoard={updateBoard}>
+                {square}
+              </Square>
+            )
+          })
+        }
+      </section>
+      
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+      
+      <WinerModal resetGame={resetGame} winner={winner}/>
+      
+    </main>
   )
 }
 
